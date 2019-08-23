bf2ab27eae55533b7e82a914dff95f93814a6a3a
Matthias Klass <matthias.klass@gmail.com>
2019-08-23 20:05:51 +0200

    .

--- game.spec.js
+++ game.spec.js
@@ -3,7 +3,7 @@ const simulateGame = require('./game');
 describe('game', () => {
     const print = jest.fn();
 
-    it('should', () => {
+    it('should print something', () => {
         simulateGame(10, print);
 
         expect(print.mock.calls[0][0]).toBe("configure me");
