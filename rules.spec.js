3db1824fb31ff47955e45c9c1a484331e1bfc773
Marco Emrich <marco.emrich@external.flixbus.com>
2019-08-23 19:56:00 +0200

    .

--- rules.spec.js
+++ rules.spec.js
@@ -8,4 +8,10 @@ describe('Dead Cell', () => {
   });
 });
 
-const willBeAlive = (aliveNeighborCount) => aliveNeighborCount === 3;
+describe('Alive Cell', () => {
+  it('stays alive with 2 living neighbours', () => {
+    expect(willBeAlive(2, true)).toEqual(true);
+  });
+})
+
+const willBeAlive = (aliveNeighborCount, isAlive) => isAlive ? true : aliveNeighborCount === 3;
