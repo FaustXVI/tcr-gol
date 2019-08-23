50f87cad6ebe4a26ef238080169717a21e1498ea
tatyango <test@test>
2019-08-23 20:28:22 +0200

    .

--- BoardPrinter.spec.js
+++ BoardPrinter.spec.js
@@ -1,13 +1,13 @@
 const Board = require('./Board');
 
-function printBoard() {
-    return '1';
+function printBoard(board) {
+    return '';
 }
 
 describe('Board toString', () => {
     it('should empty board result in empty string', () => {
         const board = new Board(0, 0);
-        var boardString = printBoard();
-        expect(boardString).toEqual('1')
+        var boardString = printBoard(board);
+        expect(boardString).toEqual('')
     });
 });
